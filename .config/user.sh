012f300e50c4a622a249f648b0f86315138f6ffe
0sec <99223413+0nsec@users.noreply.github.com>
2024-04-02 03:02:11 +0500

    Update user.sh

--- .config/user.sh
+++ .config/user.sh
@@ -1,5 +1,6 @@
 xfconf-query -c xsettings -p /Net/ThemeName -s "ctOS"
 xfconf-query -c xfwm4 -p /general/theme -s "ctOS"
+#make sure edit "/home/0nsec" and add your username
 xfce4-panel-profiles load /home/0nsec/.local/share/xfce4-panel-profiles/ctOS.tar.bz2
 sudo killall xfce4-panel
 xfce4-panel
