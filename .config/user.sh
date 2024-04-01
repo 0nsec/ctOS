bb58d22896642917b1b3ecc0dd640a388f64d562
0sec <99223413+0nsec@users.noreply.github.com>
2024-04-02 02:59:42 +0500

    Update user.sh

--- .config/user.sh
+++ .config/user.sh
@@ -1,5 +1,5 @@
 xfconf-query -c xsettings -p /Net/ThemeName -s "ctOS"
 xfconf-query -c xfwm4 -p /general/theme -s "ctOS"
-xfce4-panel-profiles load /home/craven/.local/share/xfce4-panel-profiles/ctOS.tar.bz2
+xfce4-panel-profiles load /home/0nsec/.local/share/xfce4-panel-profiles/ctOS.tar.bz2
 sudo killall xfce4-panel
 xfce4-panel
