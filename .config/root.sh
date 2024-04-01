232277de2407a6b55a80ddd18b326437907e1c7f
0sec <99223413+0nsec@users.noreply.github.com>
2024-04-02 03:01:12 +0500

    Update root.sh

--- .config/root.sh
+++ .config/root.sh
@@ -1,3 +1,4 @@
 xfconf-query -c xsettings -p /Net/ThemeName -s "root"
 xfconf-query -c xfwm4 -p /general/theme -s "root"
-xfce4-panel-profiles load /home/craven/.local/share/xfce4-panel-profiles/root.tar.bz2
+#make sure edit "/home/0nsec" and add your username
+xfce4-panel-profiles load /home/0nsec/.local/share/xfce4-panel-profiles/root.tar.bz2
