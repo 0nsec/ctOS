108226b7234fc6d3b50bf6b908aeea216d175e93
0sec <99223413+0nsec@users.noreply.github.com>
2024-04-03 11:05:00 +0500

    Update pwn.sh

--- pwn.sh
+++ pwn.sh
@@ -1,45 +1,45 @@
 clear
-echo "Loading..."
+echo "[+] Loading Malware..."
 
 echo "
 ---------------------------------------------------
-=                                                 =
-=              ctOS 2.0 San Francisco             =
-=            (c) 2016 BLUME CORPORATION           =
-=                                                 =
++++++++++++++++++++++++++++++++++++++++++++++++++++
+ ██████╗ ███╗   ██╗███████╗███████╗ ██████╗
+ ██╔═████╗████╗  ██║██╔════╝██╔════╝██╔════╝
+ ██║██╔██║██╔██╗ ██║███████╗█████╗  ██║     
+ ████╔╝██║██║╚██╗██║╚════██║██╔══╝  ██║     
+ ╚██████╔╝██║ ╚████║███████║███████╗╚██████╗
+  ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚══════╝ ╚═════╝
+ =========0nsec http://github.com/0nsec============
 ---------------------------------------------------
 "
+echo " Welcome to dedsec!!!!!!!!!! "
 echo " "
-echo " "
-echo "LOADING SHELL..."
-echo "c:\ exec pwn.sh -system "
-echo "ATTACKING...."
-echo "PROGRESS |#----------------|  beginning..."
+echo "[+] LOADING SHELL..."
+echo "[+] c:\ exec pwn.sh -system "
+echo "[+] ATTACKING...."
+echo "[+] PROGRESS |#----------------|  beginning..."
 cd Theme
 sudo cp -r ctOS /usr/share/themes
-echo "PROGRESS |##---------------|  copying system files..."
+echo "[+] PROGRESS |##---------------|  copying system files..."
 cd -
 sudo cp -r icons /usr/share/
-echo "PROGRESS |###--------------|  copying system files..."
-echo "PROGRESS |#####------------|  copying system files..."
-echo "PROGRESS |########---------|  copying system files..."
+echo "[+] PROGRESS |###--------------|  copying system files..."
+echo "[+] PROGRESS |#####------------|  copying system files..."
+echo "[+] PROGRESS |########---------|  copying system files..."
 cd root/Theme
 sudo cp -r root /usr/share/themes
-echo "PROGRESS |##########-------|  copying system files..."
+echo "[+] PROGRESS |##########-------|  copying system files..."
 cd -
 cd root/Icons
 sudo cp -r root /usr/share/icons
 cd -
-echo "PROGRESS |############-----|  copying system files..."
+echo "[+] PROGRESS |############-----|  copying system files..."
 cp -r .config ~/.config
 cp -r .local ~/.local
-echo "PROGRESS |##############---|  copying system files..."
-echo "PWNING COMPLETE LULZ"
+echo "[+] PROGRESS |##############---|  copying system files..."
+echo "[+] PWNING COMPLETE LULZ"
 echo " "
 echo " "
-echo "FOR COMPLETE CONTROL, RUN begin.sh"
-echo "(DEDSEC) Join Us."
-
-
-
-
+echo "[-] FOR COMPLETE CONTROL, RUN begin.sh"
+echo "[+] (DEDSEC) Join Us !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
